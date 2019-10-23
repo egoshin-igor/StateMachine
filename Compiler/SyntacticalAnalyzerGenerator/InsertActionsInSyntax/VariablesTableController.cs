b7f2f40ca9921d29238ba1ca663244587e9106e3
Ilya Protasov <ilya.protasov@omega-r.com>
2019-10-23 21:00:16 +0300

    - added VariablesTableController

--- Compiler/SyntacticalAnalyzerGenerator/InsertActionsInSyntax/VariablesTableController.cs
+++ Compiler/SyntacticalAnalyzerGenerator/InsertActionsInSyntax/VariablesTableController.cs
@@ -1,12 +1,45 @@
 ﻿using Lekser;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SyntacticalAnalyzerGenerator.InsertActionsInSyntax
 {
-    public interface IVariablesTableController
-    {
-        void CreateTable();
-        void InsertToCurrentTable( Term term );
-        void DestroyLastTable();
-        Term GetTerm( int id );
-    }
+	public class VariablesTableController : IVariablesTableController
+	{
+		private List<List<Term>> _tables = new List<List<Term>>();
+		private int _lastTableIndex = -1;
+
+		public void CreateTable()
+		{
+			_tables.Add(new List<Term>());
+			_lastTableIndex = _tables.Count - 1;
+		}
+
+		public void DestroyLastTable()
+		{
+			_tables.RemoveAt(_lastTableIndex);
+			_lastTableIndex--;
+		}
+
+		public Term GetTerm(int id)
+		{
+			var tableIndex = _lastTableIndex;
+
+			Term term = null;
+			while ( tableIndex != -1 )
+			{
+				term = _tables[tableIndex].FirstOrDefault(t => t.Id == id );
+				tableIndex--;
+
+				if (term != null) break;
+			}
+
+			return term;
+		}
+
+		public void InsertToCurrentTable(Term term)
+		{
+			_tables[_lastTableIndex].Add(term);
+		}
+	}
 }
